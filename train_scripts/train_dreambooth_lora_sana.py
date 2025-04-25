2308d3e91873b96f993dc9bc8cd7c934231e13ec
Raman1121 <happy07.dutt@gmail.com>
2025-04-25 13:58:46 +0100

    lora training sana

--- train_scripts/train_dreambooth_lora_sana.py
+++ train_scripts/train_dreambooth_lora_sana.py
@@ -350,7 +350,10 @@ def parse_args(input_args=None):
         help="whether to randomly flip images horizontally",
     )
     parser.add_argument(
-        "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
+        "--train_batch_size", type=int, default=16, help="Batch size (per device) for the training dataloader."
+    )
+    parser.add_argument(
+        "--eval_batch_size", type=int, default=16, help="Batch size (per device) for the eval dataloader."
     )
     parser.add_argument("--sample_batch_size", type=int, default=4, help="Batch size (per device) for sampling images.")
     parser.add_argument("--num_train_epochs", type=int, default=1)
