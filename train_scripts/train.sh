0eb5369174f69b5579b16e2a7b5315cb7cf34b5e
Raman1121 <happy07.dutt@gmail.com>
2025-05-17 01:16:27 +0100

    mod

--- train_scripts/train.sh
+++ train_scripts/train.sh
@@ -1,15 +1,17 @@
 #/bin/bash
 set -e
 
-work_dir=output/debug
-np=1
+RESOLUTION=512
+SIZE=1600M
+work_dir=output/Sana_${SIZE}_${RESOLUTION}
+np=2
 
 
 if [[ $1 == *.yaml ]]; then
     config=$1
     shift
 else
-    config="configs/sana_config/1024ms/Sana_1600M_img1024.yaml"
+    config="configs/sana_config/${RESOLUTION}ms/Sana_${SIZE}_img${RESOLUTION}.yaml"
     # config="configs/sana1-5_config/1024ms/Sana_1600M_1024px_AdamW_fsdp.yaml"      FSDP config file
     echo "Only support .yaml files, but get $1. Set to --config_path=$config"
 fi
@@ -20,9 +22,9 @@ TRITON_PRINT_AUTOTUNING=1 \
         --config_path=$config \
         --data.data_dir="[/pvc/MIMIC_ARRANGED/Train]" \
         --data.type=SanaImgDataset \
-        --model.load_from="hf://Efficient-Large-Model/Sana_1600M_512px/checkpoints/Sana_1600M_512px.pth" \
+        --model.load_from="hf://Efficient-Large-Model/Sana_${SIZE}_${RESOLUTION}px/checkpoints/Sana_${SIZE}_${RESOLUTION}px.pth" \
         --model.multi_scale=false \
-        --train.train_batch_size=256 \
+        --train.train_batch_size=64 \
         --work_dir=$work_dir \
         --name=tmp \
         --report_to=tensorboard \
