45784667516699986e72a62b0c1c0275823b553a
Raman1121 <happy07.dutt@gmail.com>
2025-07-01 09:51:35 +0530

    inference_mimic.sh

--- inference_mimic.sh
+++ inference_mimic.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-CONFIG_FILE="/pvc/Sana/configs/sana_config/512ms/Sana_1600M_img512.yaml"
-CKPT_PATH="/pvc/Sana/output/debug/checkpoints/epoch_20_step_178057.pth"
-PROMPTS_TEXT_FILE="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/mimic_test_prompts.txt"
+RESOLUTION=512
+SIZE=1600
+
+CONFIG_FILE="/pvc/Sana/configs/sana_config/${RESOLUTION}ms/Sana_${SIZE}M_img${RESOLUTION}.yaml"
+CKPT_PATH="/pvc/Sana/output/PulmoGen_1600M_512/checkpoints/epoch_15_step_20941.pth"
+# PROMPTS_TEXT_FILE="/pvc/MIMIC_Dataset/physionet.org/files/mimic-cxr-jpg/2.0.0/LLavA-Rad-Annotations/ANNOTATED_CSV_FILES/mimic_test_prompts.txt"
+PROMPTS_TEXT_FILE="/pvc/Benchmarking-Synthetic-Data/MIMIC_Splits/LLAVARAD_ANNOTATIONS_TEST.txt"
 
 python scripts/inference.py \
         --config=$CONFIG_FILE \
