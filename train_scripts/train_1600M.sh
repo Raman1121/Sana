9be343862109d77f1e0a6ac5d5edbd9c7337f29a
Raman1121 <happy07.dutt@gmail.com>
2025-07-04 11:35:54 +0530

    large scale training

--- train_scripts/train_1600M.sh
+++ train_scripts/train_1600M.sh
@@ -23,11 +23,10 @@ TRITON_PRINT_AUTOTUNING=1 \
         --data.type=SanaImgDataset \
         --model.load_from="hf://Efficient-Large-Model/Sana_${SIZE}_${RESOLUTION}px/checkpoints/Sana_${SIZE}_${RESOLUTION}px.pth" \
         --model.multi_scale=false \
-        --train.train_batch_size=64 \
+        --train.train_batch_size=96 \
         --work_dir=$work_dir \
         --tracker_project_name="PulmoGen-Training" \
         --name=PulmoGen_${SIZE}_${RESOLUTION} \
-        --report_to=wandb \
         --debug=false \
         "$@"
 
